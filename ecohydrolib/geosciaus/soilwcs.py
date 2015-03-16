9cd539c4e23ac28bd83afb681341a59b19933a22
Brian Miles <selimnairb@gmail.com>
2015-03-15 22:02:32 -0400

    Implement initial download of raw soil layers via WCS

--- ecohydrolib/geosciaus/soilwcs.py
+++ ecohydrolib/geosciaus/soilwcs.py
@@ -33,19 +33,30 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 @author Brian Miles <brian_miles@unc.edu>
 """
+import os
+import tempfile
+import shutil
+
 from owslib.wcs import WebCoverageService
 
 FORMAT_GEOTIFF = 'GeoTIFF'
 MIME_TYPE = {FORMAT_GEOTIFF: 'image/GeoTIFF'}
 
-URL_BASE_CLAY = 'http://www.asris.csiro.au/ArcGis/services/TERN/CLY_ACLEP_AU_TRN_N/MapServer/WCSServer'
-COVERAGES_CLAY = ['CLY_000_005_EV_N_P_AU_TRN_N_1',  # 0-5cm 
-                  'CLY_005_015_EV_N_P_AU_TRN_N_4',  # 5-15cm
-                  'CLY_015_030_EV_N_P_AU_TRN_N_7',  # 15-30cm
-                  'CLY_030_060_EV_N_P_AU_TRN_N_10', # 30-60cm
-                  'CLY_060_100_EV_N_P_AU_TRN_N_13', # 60-100cm
-                  ]
+# VARIABLE = {'clay': 'CLY',
+#             'silt': 'SLT',
+#             'sand': 'SND'
+#             }
 
+VARIABLE = {'clay': 'CLY'}
+
+URL_BASE = "http://www.asris.csiro.au/ArcGis/services/TERN/{variable}_ACLEP_AU_TRN_N/MapServer/WCSServer"
+
+COVERAGES = ["{variable}_000_005_EV_N_P_AU_TRN_N_1",  # 0-5cm 
+             "{variable}_005_015_EV_N_P_AU_TRN_N_4",  # 5-15cm
+             "{variable}_015_030_EV_N_P_AU_TRN_N_7",  # 15-30cm
+             "{variable}_030_060_EV_N_P_AU_TRN_N_10", # 30-60cm
+             "{variable}_060_100_EV_N_P_AU_TRN_N_13", # 60-100cm
+            ]
 
 # Example URL: http://www.asris.csiro.au/ArcGis/services/TERN/CLY_ACLEP_AU_TRN_N/MapServer/WCSServer?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage&COVERAGE=CLY_000_005_EV_N_P_AU_TRN_N_1&FORMAT=GeoTIFF&BBOX=147.539,-37.024,147.786,-36.830&RESX=0.000277777777778&RESY=0.000277777777778&CRS=EPSG:4283&RESPONSE_CRS=EPSG:4326&INTERPOLATION=bilinear&Band=1
 # http://www.asris.csiro.au/ArcGis/services/TERN/CLY_ACLEP_AU_TRN_N/MapServer/WCSServer?SERVICE=WCS&REQUEST=GetCoverage&VERSION=1.0.0&COVERAGE=1&FORMAT=GeoTIFF&BBOX=147.539,-37.024,147.786,-36.830&RESX=0.000277777777778&RESY=0.000277777777778&CRS=EPSG:4283&RESPONSE_CRS=EPSG:4326&INTERPOLATION=bilinear&Band=1
@@ -53,28 +64,45 @@ COVERAGES_CLAY = ['CLY_000_005_EV_N_P_AU_TRN_N_1',  # 0-5cm
 # Silt: http://www.asris.csiro.au/ArcGis/services/TERN/SLT_ACLEP_AU_TRN_N/MapServer/WCSServer?SERVICE=WCS&REQUEST=GetCapabilities
 # Sand: http://www.asris.csiro.au/ArcGis/services/TERN/SND_ACLEP_AU_TRN_N/MapServer/WCSServer?SERVICE=WCS&REQUEST=GetCapabilities
 
-def _getCoverageIDsForCoverageTitle(wcs, coverages_of_interest):
+def _getCoverageIDsForCoverageTitle(wcs, variable):
     coverages = wcs.items()
     coverage_ids = {}
     for coverage in coverages:
         #print('id: %s, title %s' % (coverage[0], coverage[1].title))
         id = coverage[0]
         title = coverage[1].title
-        for c in coverages_of_interest:
+        for c in COVERAGES:
+            c = c.format(variable=variable)
             if title == c:
                 coverage_ids[c] = id
-        
+         
     return coverage_ids
 
 def getSoilsRasterDataForBoundingBox(config, outputDir, bbox, srs='EPSG:4326', fmt=FORMAT_GEOTIFF, overwrite=True):
     """
     
     """
+    tmpdir = tempfile.mkdtemp()
+    print(tmpdir)
     
-    # Get % clay layers
-    wcs = WebCoverageService(URL_BASE_CLAY)
-    coverage_ids = _getCoverageIDsForCoverageTitle(wcs, COVERAGES_CLAY)
+    for v in VARIABLE.keys():
+        variable = VARIABLE[v]
+        url = URL_BASE.format(variable=variable)
+
+        wcs = WebCoverageService(url, version='1.0.0')
+        coverages = _getCoverageIDsForCoverageTitle(wcs, variable)
+        
+        for c in coverages.keys():
+            coverage = coverages[c]
+            #coverage = c.format(variable=variable)
+            wcsfp = wcs.getCoverage(identifier=coverage, bbox=bbox, format=fmt)
+            filename = os.path.join(tmpdir, "{coverage}.tif".format(coverage=coverage))
+            f = open(filename, 'wb')
+            f.write(wcsfp.read())
+            f.close()
+            
     
-    # 0-5cm
+    # Clean-up
+    #shutil.rmtree(tmpdir)
         
         
