a3be673ff9651cff2b95caa4ed9d422ce8b2d303
Brian Miles <selimnairb@gmail.com>
2015-07-02 17:37:51 -0400

    1.30.dev1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='ecohydrolib',
-      version='1.29',
+      version='1.30.dev1',
       description='Libraries and command-line scripts for performing ecohydrology data preparation workflows.',
       long_description=readme(),
       classifiers=[
